3e915080d07632a8534028ba2535384fdc05c551
Nikita Shmakov <69199813+n1k-shmak0v@users.noreply.github.com>
2021-07-02 14:37:56 +0300

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -1,19 +1,17 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-using System.IO;
-using System.Diagnostics;
+using System.Management;
+using Microsoft.Win32;
 
-namespace HDD_InfoTest
+namespace HDDInfo
 {
     public partial class Form1 : Form
     {
-        string path = "TestFile";
         public Form1()
         {
             InitializeComponent();
@@ -21,77 +19,34 @@ namespace HDD_InfoTest
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            DriveInfo[] allDrives = DriveInfo.GetDrives();
-            foreach (DriveInfo masDrive in allDrives)
-            {
-                cmbHdd.Items.Add(masDrive.Name);
-            }
-            for (int i = 1; i < 1025; i++)
-                comboBox1.Items.Add(i);
-        }
+            ManagementObjectSearcher mosDisks = new ManagementObjectSearcher("SELECT * FROM Win32_DiskDrive");
 
-        private void CmbHdd_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            DriveInfo[] allDrives = DriveInfo.GetDrives();
-            for (int i = 0; i < allDrives.Length; i++)
+            foreach (ManagementObject moDisk in mosDisks.Get())
             {
-                if (allDrives[i].Name == cmbHdd.Text)
-                {
-                    lblType.Text = "Тип диска: " + allDrives[i].DriveType;
-                    label2.Text = "Тип файловой системы: " + allDrives[i].DriveFormat;
-                    if (allDrives[i].VolumeLabel == "")
-                        label3.Text = "Метка диска: отсутствует";
-                    else
-                        label3.Text = "Метка диска: " + allDrives[i].VolumeLabel;
-                    label4.Text = "Обьем диска: " + allDrives[i].TotalSize + " Байт";
-                    label5.Text = "Свободный обьем диска: " + allDrives[i].TotalFreeSpace + " Байт";
-                    label6.Text = "Занятый обьем диска: " + (allDrives[i].TotalSize - allDrives[i].TotalFreeSpace) + " Байт";
-                }
+                cmbHdd.Items.Add(moDisk["Model"].ToString());
             }
         }
 
-        private void Button1_Click(object sender, EventArgs e)
+        private void cmbHdd_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (comboBox1.Text != "МегаБайт")
+            ManagementObjectSearcher mosDisks = new ManagementObjectSearcher("SELECT * FROM Win32_DiskDrive WHERE Model = '" + cmbHdd.SelectedItem + "'");
+            foreach (ManagementObject moDisk in mosDisks.Get())
             {
-                Stopwatch sh = new Stopwatch();
-                string a;
-                int aLen;
-                if (radioButton2.Checked)
-                { a = new string('a', Convert.ToInt32(radioButton2.Text)); aLen = Convert.ToInt32(radioButton2.Text); }
-                else { a = new string('a', Convert.ToInt32(radioButton1.Text) * 1048576); aLen = Convert.ToInt32(radioButton1.Text) * 1048576; }
-                int Mb = Convert.ToInt32(comboBox1.Text) * 1048576;
-                for (int i = 0; i < 4; i++)
-                {
-                    sh.Reset();
-                    sh.Start();
-                    StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, path), false, Encoding.ASCII);
-                    for (int j = 1; (j * aLen) <= Mb; j++)
-                        sw.Write(a);
-                    sh.Stop();
-                    sw.Close();
-                    string WriteRes = string.Format("Тест {0}: = {1:f} Мбайт/сек", (i + 1), Mb / 1024 / 1024 / sh.Elapsed.TotalSeconds);
-                    listBox1.Items.Add(WriteRes);
-                }
-                sh.Reset();
-                for (int i = 0; i < 4; i++)
-                {
-                    sh.Reset();
-                    sh.Start();
-                    FileStream fs = new FileStream(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, path), FileMode.Open, FileAccess.Read);
-                    StreamReader sr = new StreamReader(fs);
-                    long Mbr = new FileInfo(path).Length;
-                    for (int j = 0; j < Mbr; j += aLen)
-                    {
-                        char[] buffer = new char[aLen];
-                        sr.ReadBlock(buffer, 0, aLen);
-                    }
-                    sr.Close();
-                    fs.Close();
-                    sh.Stop();
-                    string ReadRes = string.Format("Тест {0}: = {1:f} Мбайт/сек", (i + 1), Mbr / 1024 / 1024 / sh.Elapsed.TotalSeconds);
-                    listBox2.Items.Add(ReadRes);
-                }
+                lblType.Text = "Type: " + moDisk["MediaType"].ToString();
+                lblModel.Text = "Model: " + moDisk["Model"].ToString();
+                lblSerial.Text = "Serial: " + moDisk["SerialNumber"].ToString();
+                lblInterface.Text = "Interface: " + moDisk["InterfaceType"].ToString();
+                lblCapacity.Text = "Capacity: " + moDisk["Size"].ToString() + " bytes (" + Math.Round(((((double)Convert.ToDouble(moDisk["Size"]) / 1024) / 1024) / 1024), 2) + " GB)";
+                lblPartitions.Text = "Partitions: " + moDisk["Partitions"].ToString();
+                lblSignature.Text = "Signature: " + moDisk["Signature"].ToString();
+                lblFirmware.Text = "Firmware: " + moDisk["FirmwareRevision"].ToString();
+                lblCylinders.Text = "Cylinders: " + moDisk["TotalCylinders"].ToString();
+                lblSectors.Text = "Sectors: " + moDisk["TotalSectors"].ToString();
+                lblHeads.Text = "Heads: " + moDisk["TotalHeads"].ToString();
+                lblTracks.Text = "Tracks: " + moDisk["TotalTracks"].ToString();
+                lblBytesPerSect.Text = "Bytes per Sector: " + moDisk["BytesPerSector"].ToString();
+                lblSectorsPerTrack.Text = "Sectors per Track: " + moDisk["SectorsPerTrack"].ToString();
+                lblTracksPerCyl.Text = "Tracks per Cylinder: " + moDisk["TracksPerCylinder"].ToString();
             }
         }
     }
